03faeab4dc8ffe7ca9e05bb2a84683839869670a
Artem <artem.andreyev212@gmail.com>
2024-01-24 20:14:15 +0200

    Added some modifications to script.js added some changes file script_test.js because of  testing different scripts

--- js/script_test.js
+++ js/script_test.js
@@ -1,3 +1,5 @@
+// ЦИКЛЫ FOR!!!
+
 // let result = "";
 // const length = 7;
 
@@ -70,3 +72,98 @@
 //     myArray[i - 5] = i;
 // }
 // console.log(myArray);
+
+// ФУНКЦИИ !!!
+
+// let num = 20;
+
+// function ShowFirstMessage(text) {
+//     console.log(text);
+//     console.log(num);
+// }
+
+// ShowFirstMessage("Hello world!");
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log("Hello!");
+// };
+
+// logger();
+
+// const calc1 = (a, b) => {
+//     console.log("1");
+//     return(a + b);
+// };
+
+// console.log(calc1(4, 6));
+
+// return в обьектах!!!
+
+// const usdCurr = 28;
+// const eurCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion (result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return;
+//     }
+//     console.log("Done"); // Функция return остановила весь код а не только цикл for как с break
+// }
+
+// test();
+
+// Методы и свойства строк и чисел
+// function doNothing() {}
+// console.log(doNothing() === undefined);
+
+// const str = "test";
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// const fruit = "Some fruit";
+
+// console.log(fruit.indexOf("fruit"));
+
+// const logg = "Hello world";
+
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+// const num = 12.2;
+// console.log(Math.round(num));
+
+// const test = "12.2px";
+// console.log(parseInt(test));
+// console.log(parseFloat(test));
+
