3c58abb3477e5d89d152ec582a3c5075dc0ead0a
Artem <artem.andreyev212@gmail.com>
2024-01-30 20:29:58 +0200

    Added some modifications to script.js (function methods), added training scripts to script_test.js

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da6f58c2b987a0049d7a1ef1e7c031e21f7f0e00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a75eed9abe9619142a5e8a042bf9e70888412b")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- js/script_test.js
+++ js/script_test.js
@@ -183,6 +183,8 @@
 // first();
 // second();
 
+// Фишки с функциями и callback!!!
+
 // function learnJS(lang, callback) {
 //     console.log(`Я учу: ${lang}`);
 //     callback();
@@ -194,34 +196,34 @@
 
 // learnJS("JavaScript", done);
 
-const options = {
-    name: "test",
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: "black",
-        bg: "red"
-    },
-    makeTest: function() {
-        console.log("Test");
-    }
-};
+// const options = {
+//     name: "test",
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: "black",
+//         bg: "red"
+//     },
+//     makeTest: function() {
+//         console.log("Test");
+//     }
+// };
 
-options.makeTest();
+// options.makeTest();
 
-const {border, bg} = options.colors;
-console.log(border);
-console.log(bg);
+// const {border, bg} = options.colors;
+// console.log(border);
+// console.log(bg);
 
 // console.log(Object.keys(options).length);
 
-const market = {
-    apple: 4,
-    banana: 3,
-    oranges: 5
-};
+// const market = {
+//     apple: 4,
+//     banana: 3,
+//     oranges: 5
+// };
 
-console.log(Object.keys(market).length);
+// console.log(Object.keys(market).length);
 
 // console.log(options["colors"]["border"]);
 
@@ -243,3 +245,182 @@ console.log(Object.keys(market).length);
 //     }
 // }
 // console.log(counter);
+
+// Методы массивов!!
+
+// const arr = [2, 13, 26, 8, 10];
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// });
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));
+
+// let a = 5;
+// let b = a; // Копия переменной
+
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj; // Передает ссылку на обьект
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+// function copy (mainObj) {
+//     let objCopy = {};
+
+//     let key;
+//     for (key in mainObj) {
+//         objCopy[key] = mainObj[key];
+//     }
+
+//     return objCopy;
+// }
+
+// const numbers = {
+//     a: 2,
+//     b: 5,
+//     c: {
+//         x: 7,
+//         y: 4
+//     }
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.c.x = 10;
+// newNumbers.a = 10;
+
+// console.log(newNumbers);
+// console.log(numbers);
+
+// const add = {
+//     d: 17,
+//     e: 20
+// };
+
+// const clone = Object.assign({}, add); // Клонирует обьект
+
+// clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+// const oldArray = ["a", "b", "c"];
+// const newArray = oldArray.slice();
+
+// newArray[1] = "asdasdasd";
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ["youtube", "vimeo", "rutube"],
+//       blogs = ["wordpress", "livejournal", "blogger"],
+//       internet = [...video, ...blogs, "vk", "facebook"];
+
+// console.log(internet);
+
+// function log(a, b, c) {
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+// const array = ["a", "b"];
+
+// const newAarray = [...array];
+
+// const q = {
+//     one: 1,
+//     two: 2
+// };
+
+// const newObj = {...q};
+
+// Упражнение по написанию кода 11, массивы
+
+// function showFamily (family) {
+//     if (family == "") {
+//         console.log("Семья пуста");
+//     }
+//     else {
+//         console.log(`Семья состоит из: ${family}`);
+//     }
+// }
+
+// const family = ["Peter", "Ann", "Alex", "Linda"];
+// let newFamily = family.slice();
+// newFamily = [];
+// console.log(family);
+// console.log(newFamily);
+
+// showFamily(family);
+// showFamily(newFamily);
+
+// Прототипы и ООП!!!
+
+// let str = "some";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1, 2, 3]);
+
+// const soldier = {
+//     healt: 400,
+//     armor: 100,
+//     sayHello: function() {
+//         console.log("Hello");
+//     }
+// };
+
+// const john = Object.create(soldier);
+
+// const john = {
+//     health: 100
+// };
+
+// john.__proto__ = soldier; // Устаревший метод!
+
+// Object.setPrototypeOf(john, soldier);
+
+// john.sayHello();
+
