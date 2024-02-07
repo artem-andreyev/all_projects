510d8b83f86f15e241ad9ba6cb82929c5f894c87
Artem <artem.andreyev212@gmail.com>
2024-02-07 21:43:00 +0200

    Working with elements on the page (modified training.js)

--- js_test/training.js
+++ js_test/training.js
@@ -1,21 +1,19 @@
 const box = document.getElementById("box");
-console.log(box);
-
+// console.log(box);
 const btns = document.getElementsByTagName("button");
-console.log(btns[0]);
-
+// console.log(btns[0]);
 const circles = document.getElementsByClassName("circle");
-console.log(circles);
+// console.log(circles);
 
 // Новые елементы
 
-const hearts = document.querySelectorAll(".heart"); // Все елементы
-hearts.forEach(item => {
-    console.log(item);
-});
-
-const oneHeart = document.querySelector(".heart"); // Первый елемент
-console.log(oneHeart);
+const wrapper = document.querySelector(".wrapper");
+const hearts = wrapper.querySelectorAll(".heart"); // Все елементы
+// hearts.forEach(item => {
+//     console.log(item);
+// });
+const oneHeart = wrapper.querySelector(".heart"); // Первый елемент
+// console.log(oneHeart);
 
 // box.style.backgroundColor = "blue";
 // box.style.width = "500px";
@@ -37,3 +35,27 @@ const div = document.createElement("div");
 // const text = document.createTextNode("Тут был я");
 
 div.classList.add("black");
+
+// document.body.append(div);
+// wrapper.appendChild(div);
+
+wrapper.append(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div); 
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = "<h1>Hello world</h1>";
+
+// div.textContent = "<h1>Hello world</h1>";
+
+div.insertAdjacentHTML("afterend", "<h2>Hello</h2>"); // beforebegin(перед елементом), afterbegin(в начало елемента), beforeend(перед концом), afterend(после елемента)
